c2053e921257572c44d1516c63062382c31fa3aa
wwwkr <xodn1003@naver.com>
2023-06-21 09:19:55 +0900

    Koin 적용

---
+++ app/src/main/java/com/example/firebasetestapp/Module.kt
@@ -0,0 +1,9 @@
+package com.example.firebasetestapp
+
+import com.example.firebasetestapp.ui.main.viewmodel.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val viewModelModule = module {
+    viewModel { MainViewModel() }
+}
